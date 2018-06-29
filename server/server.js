5b525de6b4d1cf38e8a1aa0a2a573847133c16c0
w3bh4ck <amadiluckysampson@gmail.com>
2018-06-29 16:49:07 +0100

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT | 4444;
 
 let express = require('express');
-
-
-
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+
 app.use(express.static(publicPath));
 
 
-app.get('public');
+app.get('public'); 
 
+io.on('connection', (socket) => {
+    console.log("connected");
 
-app.listen(port, ()=>{
+    socket.on('disconnect', () =>{
+        console.log('disconnected');   
+    })
+
+})
+
+server.listen(port, ()=>{
     console.log(`app listening on port ${port}`);
 })
